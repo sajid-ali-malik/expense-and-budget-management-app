4562af273449f0ed97bc7284a8d340d137c3e600
Sajid Ali <thesajidali1997@gmail.com>
2024-06-12 03:01:07 +0500

    authentications were gone through and done for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+
   has_many :accounts, dependent: :destroy
   has_many :budgets, dependent: :destroy
 
