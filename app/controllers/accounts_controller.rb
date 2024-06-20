ed80b900baf7ac039318b087b3fd23702077350b
Sajid Ali <thesajidali1997@gmail.com>
2024-06-20 17:29:57 +0500

    comment addressed

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,9 @@
 class AccountsController < ApplicationController
+
   before_action :authenticate_user!
   before_action :set_account, only: [:show, :edit, :update, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :excep
 
   def index
     @accounts = current_user.accounts
@@ -33,15 +36,20 @@ class AccountsController < ApplicationController
     end
   end
 
-def destroy
-  @account.destroy
-  redirect_to accounts_path, notice: 'Account was successfully deleted.'
-end
+  def destroy
+    @account.destroy
+    redirect_to accounts_path, notice: 'Account was successfully deleted.'
+  end
 
-  private
+private
 
   def set_account
     @account = current_user.accounts.find(params[:id])
+  end
+
+  def excep
+    flash[:alert] = 'The account you were looking for could not be found.'
+    redirect_to accounts_path
   end
 
   def account_params
